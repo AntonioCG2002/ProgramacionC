76f2c61dea82c69007e594922be1bc24d37bec6a
Antonio Cortés <78567128+AntonioCG2002@users.noreply.github.com>
2022-02-24 21:55:13 -0600

    Create CaF300a0.c

---
+++ CaF300a0.c
@@ -0,0 +1,7 @@
+/* imprime la tabla Fahrenheit-Celsiuspara fahr = 0, 20, ..., 300 */
+main ()
+{
+  int fahr;
+  for (fahr = 300; fahr >= 0; fahr = fahr - 20)
+    printf ("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
+}
