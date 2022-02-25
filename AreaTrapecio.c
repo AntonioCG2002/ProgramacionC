60befec90b836d4e1633b872bd494f1095e331e1
Antonio Cortés <78567128+AntonioCG2002@users.noreply.github.com>
2022-02-24 22:18:25 -0600

    Create AreaTrapecio.c

---
+++ AreaTrapecio.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+float B,b,a,h;
+printf("Base Mayor, Menor y altura");
+scanf("%f %f %f",&B,&b,&h);
+a=((B+b)*h)/2;
+printf("Area %f",a);
+    return 0;
+}
