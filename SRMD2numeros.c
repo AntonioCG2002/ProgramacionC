ad7f4989281e4f546bbf75833af2fd2602734bda
Antonio Cortés <78567128+AntonioCG2002@users.noreply.github.com>
2022-02-24 22:03:59 -0600

    Create SRMD2numeros.c

---
+++ SRMD2numeros.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+ int dato1, dato2, resultado;
+ dato1 = 10; //cambiar valores aqui
+ dato2 = 5;  //cambiar valores aqui
+ 
+ resultado = dato1 + dato2;
+ printf ("%d + %d = %d\n", dato1, dato2, resultado);
+ 
+ resultado = dato1 - dato2;
+ printf ("%d - %d = %d\n", dato1, dato2, resultado);
+ 
+ resultado = dato1 * dato2;
+ printf ("%d * %d = %d\n", dato1, dato2, resultado);
+ 
+ resultado = dato1 / dato2;
+ printf ("%d / %d = %d\n", dato1, dato2, resultado);
+    return 0;
+}
