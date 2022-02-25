6565c57b25cd65b953b1ae70ec574e2767098622
Antonio Cortés <78567128+AntonioCG2002@users.noreply.github.com>
2022-02-24 22:19:27 -0600

    Create Promedio3num.c

---
+++ Promedio3num.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+float a,b,c,d;
+printf("Valor 1, 2 y 3");
+scanf("%f %f %f",&a,&b,&c);
+d=(a+b+c)/3;
+printf("Promedio es %f", d);
+    return 0;
+}
