dc3988d3f480063f13e87232239cdc344c6f16ca
Antonio Cortés <78567128+AntonioCG2002@users.noreply.github.com>
2022-02-24 21:56:58 -0600

    Create Copiarentradav1.c

---
+++ Copiarentradav1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/* copia la entrada a la salida; la. versiC3n */  
+  main () {
+  
+int c;
+  
+c = getchar ();
+  
+while (c!=EOF){
+      
+putchar (c);
+      
+c = getchar ();
+    
+}
+
+}
