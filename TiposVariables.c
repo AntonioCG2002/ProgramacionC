dca254ecb6905d8856c34d8a4da9012be1a70a5e
Antonio Cortés <78567128+AntonioCG2002@users.noreply.github.com>
2022-02-24 22:09:52 -0600

    Create TiposVariables.c

---
+++ TiposVariables.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+   char a='e';
+   short b=-15;
+   int c=1024;
+   unsigned int d=125;
+   long e=123456;
+   float f=12.34;
+   double g=123456.654321;
+    printf("El elemento es:%lf",g);
+    return 0;
+}
